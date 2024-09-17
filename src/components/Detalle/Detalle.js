50102732a0a465cf8ad6bd43e3afb12ddd2f9235
Marcos Gubitosi <marcosgubitosi@Marcoss-MacBook-Air-2.local>
2024-09-17 13:20:55 -0300

    cambiando api

--- src/components/Detalle/Detalle.js
+++ src/components/Detalle/Detalle.js
@@ -7,16 +7,10 @@ class Detalle extends Component {
         this.state = { peli: null }
     }
     componentDidMount (){
-        const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OGYxNjk2MDJhNDhmNDNlZmM5NWRlOTBmNWIzMjk5MSIsIm5iZiI6MTcyNjQxMzcyNy4xNzk5NTgsInN1YiI6IjY2ZTZmOTY1ZDdiY2NhNTI0ZGIwOWM1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d6Ow_K_PM0RM2Vt-KN6IdqY_PgrstVijF790b4o8L4c'
-            }
-          };
+  
           const id = this.props.match.params.id
           
-          fetch(`https://api.themoviedb.org/3/movie/${id}`, options)
+          fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=31e421d77201e7a1eefe33f85b67fa3b`)
             .then(response => response.json())
             .then(data => this.setState({peli: data}))
             .catch(err => console.error(err));
