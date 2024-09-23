0ed7317f6b615e5079cf120ca188d31b640fc695
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2024-09-23 18:24:58 -0300

    filtrado

--- src/components/MoviesGrid/MoviesGrid.js
+++ src/components/MoviesGrid/MoviesGrid.js
@@ -7,15 +7,18 @@ import Filtrado from '../Filtrado/Filtrado'
 class MoviesGrid extends Component {
     constructor(props){
         super(props)
-        this.state = { datos: [], pelisFiltradas: [], page: 1}
+        this.state = { datos: [], pelisFiltradas: [], page: 1,  isLoading: true}
         console.log(props);
         
     }
     componentDidMount (){
         // console.log(`${this.props.url}${this.state.page}`);
+        this.setState({
+          isLoading: true
+        })
           fetch(`${this.props.url}${this.state.page}`)
             .then(response => response.json())
-            .then(data => this.setState({datos: data.results}))
+            .then(data => this.setState({datos: data.results, pelisFiltradas: data.results, isLoading : false}))
             .catch(err => console.error(err));
     }
     HandleCargarMas(){ 
@@ -25,11 +28,11 @@ class MoviesGrid extends Component {
       
     }
     filtrarPeliculas(nombre){
-      const pelFiltradas = this.state.datos.filter(
-        (elemento) => elemento.title.toLowerCase().includes(nombre.toLowerCase())
+      const pelFiltradas = this.state.pelisFiltradas.filter(
+        (e) => e.title.toLowerCase().includes(nombre.toLowerCase())
       )
       this.setState({
-        pelisFiltradas: pelFiltradas
+        datos: pelFiltradas
       })
     }
     render() { 
@@ -42,18 +45,18 @@ class MoviesGrid extends Component {
           <>
             <section className='main'>
                 <h2>{this.props.title}</h2>
-                <Filtrado filtrarPeliculas={(nom) => this.filtrarPeliculas(nom)} history={this.props.history}/>
+                <Filtrado filtrarPeliculas={(nom) => this.filtrarPeliculas(nom)}/>
                 <ul className='container'>
-              {this.state.datos.length === 0 ? (
-                <h3>Cargando...</h3>
-              ) : (this.state.pelisFiltradas.length === 0 ? (
-                  pelis.map((pelicula, idx) => (
-                    <MoviesGridCard key={idx} pelicula={pelicula}/>
-                  ))
-                ) : (pelisFilt.map((pelicula, idx) => (
-                  <MoviesGridCard key={idx} pelicula={pelicula}/>
-                )))
-              )}
+              {this.state.isLoading ? (
+                  <h3>Cargando...</h3>
+              ):(this.state.datos.length === 0 ? (
+                <h3> No hay peliculas con ese filtro</h3>
+              ) : 
+              (this.state.datos.slice(0,5).map((pelicula,idx) => (
+                <MoviesGridCard key={idx} pelicula={pelicula}/>)
+              ))) }
+              
+             
               
               {this.props.limit !== 5 ? ( <button onClick={() => this.HandleCargarMas() }>Cargar +</button>): (<p></p>)}
               </ul>
